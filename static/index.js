d7c0006c20aea36e5fc4bf08671a956f36d84426
Vikash Vishwakarma <vishwakarma.vikash1601@gmail.com>
2020-04-19 23:31:19 +0530

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -1,11 +1,1 @@
-
-$('.fa').click(event=>{
-    if($('.btn').is(":visible")){
-        $(event.target).css('transform','rotate(0deg)')
-        $('.btn').slideUp();
-    }
-    else{
-        $(event.target).css('transform','rotate(45deg)')
-        $('.btn').slideDown();
-    }
-})
+window.$=$,$(".fa").click(s=>{$(".btn").is(":visible")?($(s.target).css("transform","rotate(0deg)"),$("nav").css("height","auto"),$("section").css("display","block"),$(".btn").slideUp()):($(s.target).css("transform","rotate(45deg)"),$("nav").css("height","100vh"),$("nav").css("top","0"),$("nav").css("left","0"),$("section").css("display","none"),$(".btn").slideDown())});
